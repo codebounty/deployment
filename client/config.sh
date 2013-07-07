0df402c68b81ea2ab7e4ca8e5da55c946b9f707a
Kaiwen Xu <kevin@kevxu.net>
2013-07-07 16:09:32 -0400

    Adding new config.

--- client/config.sh
+++ client/config.sh
@@ -8,14 +8,15 @@ CB_SCP="/usr/bin/scp"
 CB_REMOTE="app.codebounty.co"
 CB_SSH_PORT="22"
 CB_USER="codebounty"
-CB_GROUP="$CB_USER"         # Group CB_USER belongs to, usually same as CB_USER. Used for chown.
-CB_ROOT="/srv/codebounty"   # Server directory root
-CB_REPO="$CB_ROOT/repo"     # Directory for storing generated bundle
+CB_GROUP="$CB_USER"                             # Group CB_USER belongs to, usually same as CB_USER. Used for chown.
+CB_ROOT="/srv/codebounty"                       # Server directory root
+CB_REPO="$CB_ROOT/repo"                         # Directory for storing generated bundle
 CB_GIT_REPO="/home/codebounty/codebounty.git"   # Git repository on server
 CB_REPO_DEPLOYED="$CB_ROOT/deployed"            # Deployed symbolic link
+CB_REPO_TESTING="$CB_ROOT/testing"              # Testing symbolic
 
 # Server side admin related settings
-CB_ADMIN_USER="admin"       # Admin user who has sudo privilege
+CB_ADMIN_USER="admin"                           # Admin user who has sudo privilege
 CB_INIT_SCRIPT="/etc/init.d/codebounty"
 
 # Misc settings
