06cd57d2c2556a6bc9f66393473346d8db1a3364
Kaiwen Xu <kevin@kevxu.net>
2013-07-17 21:43:41 -0400

    Fixed git repo path.

--- client/config.sh
+++ client/config.sh
@@ -11,7 +11,7 @@ CB_USER="codebounty"
 CB_GROUP="$CB_USER"                             # Group CB_USER belongs to, usually same as CB_USER. Used for chown.
 CB_ROOT="/srv/codebounty"                       # Server directory root
 CB_REPO="$CB_ROOT/repo"                         # Directory for storing generated bundle
-CB_GIT_REPO="/home/codebounty/codebounty.git"   # Git repository on server
+CB_GIT_REPO="/home/deploymentuser/codebounty.git"   # Git repository on server
 CB_REPO_DEPLOYED="$CB_ROOT/deployed"            # Deployed symbolic link
 CB_REPO_TESTING="$CB_ROOT/testing"              # Testing symbolic
 
