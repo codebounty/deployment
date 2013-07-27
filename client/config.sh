d31dc0c7ab05892cf8e2753519d264ef76a1681a
Kaiwen Xu <kevin@kevxu.net>
2013-07-27 17:34:54 -0400

    Remove client command config testing. Minor config change.

--- client/config.sh
+++ client/config.sh
@@ -6,14 +6,15 @@ CB_SCP="/usr/bin/scp"
 
 # Server side settings
 CB_REMOTE="app.codebounty.co"
+CB_REMOTE_TES="x.codebounty.co"
 CB_SSH_PORT="22"
 CB_USER="codebounty"
 CB_GROUP="$CB_USER"                             # Group CB_USER belongs to, usually same as CB_USER. Used for chown.
 CB_ROOT="/srv/codebounty"                       # Server directory root
 CB_REPO="$CB_ROOT/repo"                         # Directory for storing generated bundle
 CB_GIT_REPO="/home/deploymentuser/codebounty.git"   # Git repository on server
-CB_REPO_DEPLOYED="$CB_ROOT/deployed"            # Deployed symbolic link
-CB_REPO_TESTING="$CB_ROOT/testing"              # Testing symbolic
+CB_REPO_DEPLOY="$CB_ROOT/deployed"            # Deployed symbolic link
+CB_REPO_TEST="$CB_ROOT/testing"              # Testing symbolic
 
 # Server side admin related settings
 CB_ADMIN_USER="admin"                           # Admin user who has sudo privilege
