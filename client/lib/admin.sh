97428157ca62dbae97163f9d680a5d6058a70270
Kaiwen Xu <kevin@kevxu.net>
2013-07-28 21:03:07 -0400

    Error fix for cb-repo-link.

--- client/lib/admin.sh
+++ client/lib/admin.sh
@@ -11,12 +11,20 @@ cb_admin_ssh() {
     fi
 }
 
+cb_admin_start_server() {
+    cb_admin_ssh "sudo" "$CB_INIT_SCRIPT" "start"
+}
+
 cb_admin_restart_server() {
     cb_admin_ssh "sudo" "$CB_INIT_SCRIPT" "restart"
 }
 
 cb_admin_stop_server() {
     cb_admin_ssh "sudo" "$CB_INIT_SCRIPT" "stop"
+}
+
+cb_admin_start_test_server() {
+    cb_admin_ssh "sudo" "$CB_INIT_SCRIPT_TEST" "start"
 }
 
 cb_admin_restart_test_server() {
