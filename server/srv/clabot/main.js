0bb384f9b893584f77c02012db94f696806448ab
Kaiwen Xu <kevin@kevxu.net>
2013-08-24 16:18:44 -0400

    Start to buile basic config structure for clabot.

--- server/srv/clabot/main.js
+++ server/srv/clabot/main.js
@@ -1,4 +1,12 @@
-options = {};
+options = {
+    getContractors: undefined,
+    token: process.env.GITHUB_TOKEN,
+    templates: undefined,
+    templateData: undefined,
+    secrets: {
+
+    }
+};
 
 app = require('clabot').createApp(options);
 app.listen(process.env.PORT || 1337);
